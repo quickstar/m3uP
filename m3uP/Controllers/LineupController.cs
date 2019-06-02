5f3d4e4f1e44e157ef5e223c68251c95d6ddea1b
Lukas Läderach <lukas.laederach@3volutions.ch>
2019-06-02 05:22:21 +0200

    implement lineup

---
+++ m3uP/Controllers/LineupController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+using m3uP.Models;
+using m3uP.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace m3uP.Controllers
+{
+	[ApiController]
+	public class LineupController : ControllerBase
+	{
+		private readonly IPlaylistParser _playlistParser;
+		private readonly IPlaylistProvider _playlistProvider;
+
+		// GET api/discovery/description
+		public LineupController(IPlaylistParser playlistParser, IPlaylistProvider playlistProvider)
+		{
+			_playlistParser = playlistParser;
+			_playlistProvider = playlistProvider;
+		}
+
+		[Produces("application/json")]
+		[HttpGet]
+		[Route("lineup.json")]
+		[Route("[controller]")]
+		public ActionResult Get()
+		{
+			var allChannels = _playlistProvider.ReadChannels();
+			var channelInfos = _playlistParser.ParseChannels(allChannels)
+					.OrderBy(c => c.GroupTitle)
+					.ThenBy(c => c.Name)
+					.Select((c, i) => new Lineup
+					{
+							GuideName = c.Name,
+							GuideNumber = (1000 + i).ToString(),
+							Hd = 1,
+							Url = c.StreamUrl.ToString()
+					})
+					.ToArray();
+
+			return new JsonResult(channelInfos, new JsonSerializerSettings
+			{
+					ContractResolver = new DefaultContractResolver()
+			});
+		}
+	}
+}
---
+++ m3uP/Models/GuideName.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace m3uP.Models
+{
+	public class Lineup
+	{
+		public string GuideName { get; set; }
+
+		public string GuideNumber { get; set; }
+
+		[JsonProperty("HD")]
+		public int Hd { get; set; }
+
+		[JsonProperty("URL")]
+		public string Url { get; set; }
+	}
+}
